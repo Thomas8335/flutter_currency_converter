cf6da671d3fed6c7fcaf4ac06ca18eeebd5be9fc
Thomas8335 <twalton2@css.edu>
2023-11-06 16:40:39 -0600

    done

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final dollarTextField = TextEditingController();
+  final euroTextField = TextEditingController();
+  double _dollars = 0;
+  double _euros = 0;
+  double _conversionRate = 1.06;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: const Text("Currency Converter"),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Dollars"),
+            TextField(controller: dollarTextField),
+
+            ElevatedButton(
+                onPressed: () {
+                  print("Convert  euros button pressed");
+                  _dollars = double.parse(dollarTextField.text);
+                  _euros = _dollars / _conversionRate;
+                  euroTextField.text = _euros.toStringAsFixed(2);
+                },
+                child: Text("Convert")),
+
+            Text("Euros"),
+            TextField(controller: euroTextField ),
+
+
+            ElevatedButton(
+                onPressed: () {
+                  print("Convert dollars button pressed");
+                  _euros = double.parse(euroTextField.text);
+                  _dollars = _euros * _conversionRate;
+                  dollarTextField.text = _dollars.toStringAsFixed(2);
+                },
+                child: Text("Convert"))
+
+          ],
+        ),
+      ),
+    );
+  }
+}
